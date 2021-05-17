f50d111f1388b07ab1d7f9bb4ca5928ab17b755f
Kevin <training@adaptablebytes.com>
2021-05-17 16:52:50 +0000

    work in progress; apply initial HTML changes

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function isPrincipalValid(amount) {
+  var val = parseFloat(amount);
+  if (val === undefined || val === NaN) {
+      return false;
+  }
+}
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+function compute() {
+    var p = document.getElementById("principal");
+    var principal = p.value();
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var year = new Date().getFullYear()+parseInt(years);
+    var interest = principal * years * rate /100;
+
 }
         
