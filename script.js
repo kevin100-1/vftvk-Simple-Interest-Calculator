d4cdeb67806c031294094b13b569faad171823f6
Kevin <training@adaptablebytes.com>
2021-05-17 17:34:11 +0000

    fix CSS

--- script.js
+++ script.js
@@ -27,5 +27,6 @@ function compute() {
     var interest = principal * years * rate /100;
 
     document.getElementById("result").innerHTML='If you deposit ' + principal + ',<br/>at an interest rate of ' + rate + '%<br/>You will receive an amount of ' + interest + ',<br/>in the year ' + year + '<br/>';
+    p.focus();
 }
         
